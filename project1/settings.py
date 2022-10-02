419110b79333adeee26d1542678ef8e059f1add8
ramymonazaa <romeo33667@gmail.com>
2022-10-02 15:56:38 +0200

    first commit

--- project1/settings.py
+++ project1/settings.py
@@ -111,8 +111,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'linky/static')
+    os.path.join(BASE_DIR,'project1/static')
     ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
