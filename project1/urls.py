489785c865b7277c399ae86b14e0f17913168e56
ramymonazaa <romeo33667@gmail.com>
2022-10-02 21:46:24 +0200

    sss

--- project1/urls.py
+++ project1/urls.py
@@ -18,8 +18,8 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.views.static import serve
-from django.urls import re_path
+# from django.views.static import serve
+# from django.urls import re_path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -28,5 +28,5 @@ urlpatterns = [
     # re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     # re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
-# + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-# urlpatterns+=staticfiles_urlpatterns()
++ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns+=staticfiles_urlpatterns()
